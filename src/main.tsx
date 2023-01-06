acee010c7d625ae3f050e6e1fe8e717da9c09c60
juaamol <jambulud@gmail.com>
2023-01-06 22:16:10 +0100

    setup redux store

--- src/main.tsx
+++ src/main.tsx
@@ -2,18 +2,22 @@ import { ThemeProvider } from '@emotion/react';
 import { CssBaseline } from '@mui/material';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 import { HashRouter as Router } from 'react-router-dom';
 import App from './App';
 import './index.css';
+import { store } from './store';
 import theme from './theme';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <Router>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <App />
-      </ThemeProvider>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <App />
+        </ThemeProvider>
+      </Router>
+    </Provider>
   </React.StrictMode>,
 );
---
+++ src/store/auth/auth-slice.ts
@@ -0,0 +1,29 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+
+interface AuthState {
+  status: 'authenticated' | 'not-authenticated' | 'checking';
+  uid: string | null;
+  email: string | null;
+  displayName: string | null;
+  photoUrl: string | null;
+}
+
+const initialState: AuthState = {
+  status: 'checking',
+  uid: null,
+  email: null,
+  displayName: null,
+  photoUrl: null,
+};
+
+export const authSlice = createSlice({
+  name: 'auth',
+  initialState,
+  reducers: {
+    login: (state, action: PayloadAction<any>) => {},
+    logout: (state, action: PayloadAction<any>) => {},
+    checkingCredentials: (state) => {},
+  },
+});
+
+export const { login, logout, checkingCredentials } = authSlice.actions;
