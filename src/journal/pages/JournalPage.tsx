f62fa5894c350b9a7fa9c4f98d5cc9191df3d2f4
juaamol <jambulud@gmail.com>
2023-01-06 19:40:05 +0100

    Add floating button to add new note

--- src/journal/pages/JournalPage.tsx
+++ src/journal/pages/JournalPage.tsx
@@ -1,12 +1,29 @@
-import Typography from '@mui/material/Typography';
 import { JournalLayout } from '../layout/JournalLayout';
 import { NoteView, NothingSelectedView } from '../views';
+import IconButton from '@mui/material/IconButton';
+import AddOutlined from '@mui/icons-material/AddOutlined';
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
-      <NoteView />
-      {/* <NothingSelectedView /> */}
+      {/* <NoteView /> */}
+      <NothingSelectedView />
+      <IconButton
+        size='large'
+        sx={{
+          color: 'white',
+          backgroundColor: 'error.main',
+          ':hover': {
+            backgroundColor: 'error.main',
+            opacity: 0.9,
+          },
+          position: 'fixed',
+          right: 50,
+          bottom: 50,
+        }}
+      >
+        <AddOutlined sx={{ fontSize: 30 }} />
+      </IconButton>
     </JournalLayout>
   );
 };
