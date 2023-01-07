1e296a09f12915e78a63955906a846fbd61f8077
juaamol <jambulud@gmail.com>
2023-01-07 18:47:03 +0100

    Validate register and login forms

--- src/auth/pages/LoginPage.tsx
+++ src/auth/pages/LoginPage.tsx
@@ -6,23 +6,24 @@ import Google from '@mui/icons-material/Google';
 import Link from '@mui/material/Link';
 import { Link as RouterLink } from 'react-router-dom';
 import { AuthLayout } from '../layout/AuthLayout';
-import { useForm } from '../../hooks/useForm';
-import { FormEvent, useMemo } from 'react';
+import { useMemo } from 'react';
+import { Formik } from 'formik';
 import { checkingAuthentication, startGoogleSignIn } from '../../store';
 import { useAppDispatch, useAppSelector } from '../../hooks';
+import { loginValidation } from '../validations';
+
+const initialValues = {
+  email: '',
+  password: '',
+};
 
 export const LoginPage = () => {
   const { status } = useAppSelector((state) => state.auth);
   const isAuthenticating = useMemo(() => status === 'checking', [status]);
   const dispatch = useAppDispatch();
-  const { formState, onInputChange } = useForm({
-    email: '',
-    password: '',
-  });
-  const { email, password } = formState;
 
-  const onSubmit = (event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
+  const onSubmit = (values: { email: string; password: string }) => {
+    const { email, password } = values;
     dispatch(checkingAuthentication(email, password));
   };
 
@@ -32,65 +33,90 @@ export const LoginPage = () => {
 
   return (
     <AuthLayout title='Login'>
-      <form onSubmit={onSubmit}>
-        <Grid container gap={2}>
-          <Grid item xs={12}>
-            <TextField
-              label='Email'
-              type='email'
-              fullWidth
-              name='email'
-              value={email}
-              onChange={onInputChange}
-            ></TextField>
-          </Grid>
-          <Grid item xs={12}>
-            <TextField
-              label='Password'
-              type='password'
-              fullWidth
-              name='password'
-              value={password}
-              onChange={onInputChange}
-            ></TextField>
-          </Grid>
+      <Formik
+        onSubmit={onSubmit}
+        initialValues={initialValues}
+        validationSchema={loginValidation}
+      >
+        {({
+          values,
+          errors,
+          touched,
+          handleBlur,
+          handleChange,
+          handleSubmit,
+        }) => (
+          <form onSubmit={handleSubmit}>
+            <Grid container gap={2}>
+              <Grid item xs={12}>
+                <TextField
+                  label='Email'
+                  type='email'
+                  fullWidth
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  value={values.email}
+                  name='email'
+                  error={!!touched.email && !!errors.email}
+                  helperText={touched.email && errors.email}
+                ></TextField>
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  label='Password'
+                  type='password'
+                  fullWidth
+                  name='password'
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  value={values.password}
+                  error={!!touched.password && !!errors.password}
+                  helperText={touched.password && errors.password}
+                ></TextField>
+              </Grid>
 
-          <Grid container spacing={2}>
-            <Grid item xs={12} sm={6}>
-              <Button
-                variant='contained'
-                fullWidth
-                type='submit'
-                disabled={isAuthenticating}
-              >
-                Login
-              </Button>
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <Button
-                variant='contained'
-                fullWidth
-                onClick={onGoogleSignIn}
-                disabled={isAuthenticating}
-              >
-                <Google /> <Typography sx={{ ml: 1 }}>Google</Typography>
-              </Button>
-            </Grid>
-          </Grid>
+              <Grid container spacing={2}>
+                <Grid item xs={12} sm={6}>
+                  <Button
+                    variant='contained'
+                    fullWidth
+                    type='submit'
+                    disabled={isAuthenticating}
+                  >
+                    Login
+                  </Button>
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  <Button
+                    variant='contained'
+                    fullWidth
+                    onClick={onGoogleSignIn}
+                    disabled={isAuthenticating}
+                  >
+                    <Google /> <Typography sx={{ ml: 1 }}>Google</Typography>
+                  </Button>
+                </Grid>
+              </Grid>
 
-          <Grid
-            container
-            spacing={2}
-            pt={1}
-            direction='row'
-            justifyContent='end'
-          >
-            <Link color='inherit' to='/auth/register' component={RouterLink}>
-              Create an account
-            </Link>
-          </Grid>
-        </Grid>
-      </form>
+              <Grid
+                container
+                spacing={2}
+                pt={1}
+                direction='row'
+                justifyContent='end'
+              >
+                <Link
+                  color='inherit'
+                  to='/auth/register'
+                  component={RouterLink}
+                >
+                  Create an account
+                </Link>
+              </Grid>
+            </Grid>
+          </form>
+        )}
+      </Formik>
     </AuthLayout>
   );
 };
--- src/auth/pages/RegisterPage.tsx
+++ src/auth/pages/RegisterPage.tsx
@@ -4,44 +4,101 @@ import Button from '@mui/material/Button';
 import Link from '@mui/material/Link';
 import { Link as RouterLink } from 'react-router-dom';
 import { AuthLayout } from '../layout/AuthLayout';
+import { Formik } from 'formik';
+import { registerValidation } from '../validations';
+
+const initialValues = {
+  email: '',
+  password: '',
+  displayName: '',
+};
 
 export const RegisterPage = () => {
+  const onSubmit = (values: any) => {
+    console.log(values);
+  };
+
   return (
     <AuthLayout title='Register'>
-      <form>
-        <Grid container gap={2}>
-          <Grid item xs={12}>
-            <TextField label='Name' type='text' fullWidth></TextField>
-          </Grid>
+      <Formik
+        onSubmit={onSubmit}
+        initialValues={initialValues}
+        validationSchema={registerValidation}
+      >
+        {({
+          values,
+          errors,
+          touched,
+          handleBlur,
+          handleChange,
+          handleSubmit,
+        }) => (
+          <form onSubmit={handleSubmit}>
+            <Grid container gap={2}>
+              <Grid item xs={12}>
+                <TextField
+                  label='Name'
+                  type='text'
+                  fullWidth
+                  name='displayName'
+                  onBlur={handleBlur}
+                  value={values.displayName}
+                  onChange={handleChange}
+                  error={!!touched.displayName && !!errors.displayName}
+                  helperText={touched.displayName && errors.displayName}
+                ></TextField>
+              </Grid>
 
-          <Grid item xs={12}>
-            <TextField label='Email' type='email' fullWidth></TextField>
-          </Grid>
-          <Grid item xs={12}>
-            <TextField label='Password' type='email' fullWidth></TextField>
-          </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  label='Email'
+                  type='email'
+                  fullWidth
+                  name='email'
+                  onBlur={handleBlur}
+                  value={values.email}
+                  onChange={handleChange}
+                  error={!!touched.email && !!errors.email}
+                  helperText={touched.email && errors.email}
+                ></TextField>
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  label='Password'
+                  type='password'
+                  fullWidth
+                  name='password'
+                  onBlur={handleBlur}
+                  value={values.password}
+                  onChange={handleChange}
+                  error={!!touched.password && !!errors.password}
+                  helperText={touched.password && errors.password}
+                ></TextField>
+              </Grid>
 
-          <Grid container spacing={2}>
-            <Grid item xs={12} sm={6}>
-              <Button variant='contained' fullWidth>
-                Crear cuenta
-              </Button>
+              <Grid container spacing={2}>
+                <Grid item xs={12} sm={6}>
+                  <Button variant='contained' fullWidth type='submit'>
+                    Crear cuenta
+                  </Button>
+                </Grid>
+              </Grid>
+
+              <Grid
+                container
+                spacing={2}
+                pt={1}
+                direction='row'
+                justifyContent='end'
+              >
+                <Link color='inherit' to='/auth/login' component={RouterLink}>
+                  Already have an account?
+                </Link>
+              </Grid>
             </Grid>
-          </Grid>
-
-          <Grid
-            container
-            spacing={2}
-            pt={1}
-            direction='row'
-            justifyContent='end'
-          >
-            <Link color='inherit' to='/auth/login' component={RouterLink}>
-              Already have an account?
-            </Link>
-          </Grid>
-        </Grid>
-      </form>
+          </form>
+        )}
+      </Formik>
     </AuthLayout>
   );
 };
